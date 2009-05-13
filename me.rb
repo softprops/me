c247429a7cf3c236f71d2776e38e57b80d046c1c
softprops <d.tangren@gmail.com>
2009-05-12 22:52:22 -0400

    made send_mail its own method and was picky with haml hash spacing

--- me.rb
+++ me.rb
@@ -1,7 +1,6 @@
 %w(sinatra/base haml pony).each { |l| require l }
 
 class Me < Sinatra::Base
-  
   EMAIL = ENV['email']
   
   set :root, File.dirname(__FILE__)
@@ -13,18 +12,22 @@ class Me < Sinatra::Base
  
   post '/' do
     if params[:message]
-      Pony.mail(:to      => EMAIL, 
-                :from    => 'curious@example.com', 
-                :subject => 'message from lessis.me', 
-                :body    => params[:message]) if ENV['production']
-                
+      send_mail
       flash[:notice] = "thanks for saying hello"
     end
+    
     redirect '/ '
   end
 
   use_in_file_templates!
   
+  private 
+    def send_mail
+      Pony.mail(:to      => EMAIL, 
+                :from    => 'curious@example.com', 
+                :subject => 'message from lessis.me', 
+                :body    => params[:message]) if ENV['production']
+    end
 end
 
 __END__
@@ -33,12 +36,12 @@ __END__
 !!!
 %html
   %head
-    %meta{:name => "keywords", :content => "doug tangren,less,lessisme,simple,code,ruby,java,blog" } 
-    %meta{:name => "description", :content => "less more and more doug" } 
-    %meta{:name => "author", :content => "Doug Tangren" } 
+    %meta{ :name => "keywords", :content => "doug tangren,less,lessisme,simple,code,ruby,java,blog" } 
+    %meta{ :name => "description", :content => "less more and more doug" } 
+    %meta{ :name => "author", :content => "Doug Tangren" } 
     %title 
       less is me
-    %link{ :type=>"text/css", :rel=>"stylesheet", :href=>"css/app.css" }
+    %link{ :type => "text/css", :rel => "stylesheet", :href => "css/app.css" }
   %body
     - if flash[:notice]
       #flash
@@ -47,56 +50,56 @@ __END__
       %li
         %h1
           hi. my name is 
-          %span{:class=>"vcard"}
-            %span{:class=>"fn"}
+          %span{ :class => "vcard" }
+            %span{ :class => "fn" }
               Doug
-            %a{:href=>"/", :class=>"url hdn"}
+            %a{ :href=>"/", :class => "url hdn" }
               doug
-            %span{:class=>"geo"}
-              %span{:class=>"latitude", :title=>"40.438651"}
+            %span{ :class => "geo" }
+              %span{ :class => "latitude", :title => "40.438651" }
                 40.777705 
-              %span{:class=>"longitude", :title=>"-79.929249"}
+              %span{ :class => "longitude", :title => "-79.929249" }
                 -73.947958
-            %a{:class=>"email", :href=>"mailto:#{Me::EMAIL}", :rel=>"email"}
-              %span{:class=>"type"}
+            %a{ :class => "email", :href => "mailto:#{Me::EMAIL}", :rel => "email" }
+              %span{ :class => "type" }
                 pref
                 (email me)
       %li
         %h1
           i share 
-          %a{:href=>"http://softprops.github.com", :rel=>"me"}
+          %a{ :href => "http://softprops.github.com", :rel => "me" }
             code
       %li
         %h1
           i sometimes
-          %a{:href=>"http://www.twitter.com/softprops", :rel=>"me"}
+          %a{ :href => "http://www.twitter.com/softprops", :rel => "me" }
             tweet
       %li
         %h1
           not much else to 
-          %a{:href=>"mailto:soft@softprops.net", :rel=>"hello"}
+          %a{ :href => "mailto:soft@softprops.net", :rel => "hello" }
             say
 	
         #new_message
-          %form{:action=>"/", :method=>"post"}
+          %form{ :action => "/", :method => "post" }
             %dl
               %dt
-                %label{:for=>"message"}
+                %label{ :for => "message" }
                   so say it for me.
               %dd
-                %textarea{:name=>"message", :class=>"growable"}
+                %textarea{ :name => "message", :class => "growable" }
                   Hi. My name is Curious.
-            %input{:type=>"submit", :value=>"say hello"} 
-            %span{:class=>"not"}
+            %input{ :type => "submit", :value => "say hello" } 
+            %span{ :class => "not" }
               or maybe 
-              %a{:href=>"#", :rel=>"not"}
+              %a{ :href => "#", :rel => "not" }
                 tomorrow
     %hr
     #footer
-      %span{:id=>"copy"}
+      %span{ :id => "copy" }
         &copy; &infin;
       %p#important
         &#150;live happily 
         %span.blue 
           &#x2764;
-    %script{:type=>"text/javascript", :src=>"js/app.js"}
+    %script{ :type => "text/javascript", :src => "js/app.js" }
